935afe54311de9479c5b9f826a623be383f0d972
qingchunshendeng <13692245232@163.com>
2021-08-17 18:28:40 +0800

    fragment start

---
+++ app/src/main/java/com/example/firstcode/chapter5/FiveActivity.kt
@@ -0,0 +1,12 @@
+package com.example.firstcode.chapter5
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.firstcode.R
+
+class FiveActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_five)
+    }
+}
