dfdb0b280c3351e3bfa95256670cff19ecf33f08
qingchunshendeng <13692245232@163.com>
2021-08-19 18:41:01 +0800

    database

---
+++ app/src/main/java/com/example/firstcode/chapter7/database/DatabaseActivity.kt
@@ -0,0 +1,12 @@
+package com.example.firstcode.chapter7.database
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.firstcode.R
+
+class DatabaseActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_database)
+    }
+}
